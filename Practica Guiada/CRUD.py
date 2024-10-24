251d1fe52495b468e3ff67b0a78d694ab2cea56e
Inti <intijuarez.prof@gmail.com>
2024-10-24 16:30:50 -0300

    Interfaz gráfica completa, comienzo de lógica.

--- Practica Guiada/CRUD.py
+++ Practica Guiada/CRUD.py
@@ -3,17 +3,83 @@ from tkinter import messagebox
 import sqlite3
 
 
+#------------Lógica, funciones----------------
+
+#Conexión BD
+def conexionBBDD():
+
+    miConexion=sqlite3.connect("Usuarios")
+
+    miCursor=miConexion.cursor()
+
+    try:
+        miCursor.execute('''
+    
+            CREATE TABLE DATOSUSUARIOS (
+            ID INTEGER PRIMARY KEY AUTOINCREMENT,
+            NOMBRE_USUARIO VARCHAR(50),
+            PASSWORD VARCHAR(50),
+            APELLIDO VARCHARR(10),
+            DIRECCION VARCHAR(50),
+            COMENTARIOS VARCHAR(100))
+            ''')
+
+        messagebox.showinfo("BBDD", "BBDD creada con éxito")
+    except:
+
+        messagebox.showinfo("Atención", "la BBDD ya existe")
+
+
+#Salir
+def salirAplicacion():
+
+    valor=messagebox.askquestion("Salir","¿Seguro desea salir de la aplicación?")
+
+    if valor=="yes":
+        root.destroy()
+
+
+
+#Limpiar campos
+def limpiarCampos():
+
+    miNombre.set("")
+    miId.set("")
+    miApellido.set("")
+    miDireccion.set("")
+    miPass.set("")
+    textoComentario.delete(1.0, END)
+
+
+#Crear
+def crear():
+    miConexion=sqlite3.connect("Usuarios")
+
+    miCursor=miConexion.cursor()
+
+    miCursor.execute("INSERT INTO DATOSUSUARIOS VALUES(NULL, '" + miNombre.get() +
+        "','" + miPass.get() +
+        "','" + miApellido.get() +
+        "'," + miDireccion.get() + 
+        "','" + textoComentario.get("1.0", END) + "')")
+
+    miConexion.commit()
+
+    messagebox.showinfo("BBDD", "Regristro insertado con éxito.")
+
+
+
 root=Tk()
 
 barraMenu=Menu(root)
 root.config(menu=barraMenu, width=300, height=300)
 
 bbddMenu=Menu(barraMenu, tearoff=0)
-bbddMenu.add_command(label="Conectar")
-bbddMenu.add_command(label="Salir")
+bbddMenu.add_command(label="Conectar", command=conexionBBDD)
+bbddMenu.add_command(label="Salir", command=salirAplicacion)
 
 borrarMenu=Menu(barraMenu, tearoff=0)
-borrarMenu.add_command(label="Borrar campos")
+borrarMenu.add_command(label="Borrar campos", command=limpiarCampos)
 
 crudMenu=Menu(barraMenu, tearoff=0)
 crudMenu.add_command(label="Crear")
@@ -36,21 +102,28 @@ barraMenu.add_cascade(label="Ayuda", menu=ayudaMenu)
 miFrame=Frame(root)
 miFrame.pack()
 
-cuadroId=Entry(miFrame)
+miId=StringVar()
+miNombre=StringVar()
+miApellido=StringVar()
+miPass=StringVar()
+miDireccion=StringVar()
+
+
+cuadroId=Entry(miFrame, textvariable=miId)
 cuadroId.grid(row=0, column=1, padx=10, pady=10)
 
-cuadroNombre=Entry(miFrame)
+cuadroNombre=Entry(miFrame, textvariable=miNombre)
 cuadroNombre.grid(row=1, column=1, padx=10, pady=10)
 cuadroNombre.config(fg="red", justify="right")
 
-cuadroPassword=Entry(miFrame)
+cuadroPassword=Entry(miFrame, textvariable=miPass)
 cuadroPassword.grid(row=2, column=1, padx=10, pady=10)
 cuadroPassword.config(show="?")
 
-cuadroApellido=Entry(miFrame)
+cuadroApellido=Entry(miFrame, textvariable=miApellido)
 cuadroApellido.grid(row=3, column=1, padx=10, pady=10)
 
-cuadroDireccion=Entry(miFrame)
+cuadroDireccion=Entry(miFrame, textvariable=miDireccion)
 cuadroDireccion.grid(row=4, column=1, padx=10, pady=10)
 
 textoComentario=Text(miFrame, width=16, height=5)
@@ -62,22 +135,22 @@ textoComentario.config(yscrollcommand=scrollVert.set)
 
 #---------------------Label-------------------------------
 
-idLabel=Label(miFrame, text="Id")
+idLabel=Label(miFrame, text="Id:")
 idLabel.grid(row=0, column=0, sticky="e", padx=10, pady=10)
 
-nombreLabel=Label(miFrame, text="Id")
+nombreLabel=Label(miFrame, text="Nombre:")
 nombreLabel.grid(row=1, column=0, sticky="e", padx=10, pady=10)
 
-passLabel=Label(miFrame, text="Id")
+passLabel=Label(miFrame, text="Password:")
 passLabel.grid(row=2, column=0, sticky="e", padx=10, pady=10)
 
-apellidoLabel=Label(miFrame, text="Id")
+apellidoLabel=Label(miFrame, text="Apellido:")
 apellidoLabel.grid(row=3, column=0, sticky="e", padx=10, pady=10)
 
-direccionLabel=Label(miFrame, text="Id")
+direccionLabel=Label(miFrame, text="Dirección:")
 direccionLabel.grid(row=4, column=0, sticky="e", padx=10, pady=10)
 
-comentariosLabel=Label(miFrame, text="Id")
+comentariosLabel=Label(miFrame, text="Comentarios:")
 comentariosLabel.grid(row=5, column=0, sticky="e", padx=10, pady=10)
 
 
