c6a5306269666a3fc0c147849ffcd29c3b48ae5b
Inti <intijuarez.prof@gmail.com>
2024-10-31 15:53:43 -0300

    Funciones decoradoras

--- Decoradores/funDeco.py
+++ Decoradores/funDeco.py
@@ -1,1 +1,33 @@
 #Las funciones decoradores añaden funcionalidades a otras funciones.
+
+def funcion_decoradora(funcion_parametro):
+
+    def funcion_interior():
+
+        #Acciones adicionales que decora
+
+        print("Vamos a realizar un cálculo: ")
+
+        funcion_parametro()
+
+        print("Hemos terminado el cálculo")
+
+    return funcion_interior
+
+
+
+
+@funcion_decoradora
+def suma():
+
+    print(15+20)
+
+
+
+
+def resta():
+
+    print(30-10)
+
+
+suma()
